f0d070df9875dc2974b612cff9ba2bddb8ca151b
Juan Carlos Collao Mamani <jccm0909@gmail.com>
2026-03-30 21:43:46 -0400

    Prevent SQL injection by migrating Database queries to prepared statements

--- models/Database.php
+++ models/Database.php
@@ -29,8 +29,16 @@ class Database {
         return static::$alerts;
     }
 
-    public static function customQuery($query, $model = false) {
-        $result = self::$db->query($query);
+    public static function customQuery($query, $model = false, $types = '', ...$params) {
+        if ($types && $params) {
+            $stmt = self::$db->prepare($query);
+            $stmt->bind_param($types, ...$params);
+            $stmt->execute();
+            $result = $stmt->get_result();
+        } else {
+            $result = self::$db->query($query);
+        }
+
         $objectsArray = [];
         while ($row = $result->fetch_assoc()) {
             $object = $model ? new static : new stdClass;
@@ -41,6 +49,7 @@ class Database {
         }
 
         $result->free();
+        if (isset($stmt)) $stmt->close();
 
         return $objectsArray;
     }
@@ -54,6 +63,27 @@ class Database {
         }
 
         $result->free();
+
+        return $array;
+    }
+
+    protected static function preparedQuery($query, $types = '', ...$params) {
+        $stmt = self::$db->prepare($query);
+
+        if ($types && $params) {
+            $stmt->bind_param($types, ...$params);
+        }
+
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $array = [];
+        while ($row = $result->fetch_assoc()) {
+            $array[] = static::createObject($row);
+        }
+
+        $result->free();
+        $stmt->close();
 
         return $array;
     }
@@ -82,15 +112,10 @@ class Database {
         return $attributes;
     }
 
-    public function sanitizeAttributes() {
-        $attributes = $this->attributes();
-        $sanitized = [];
-
-        foreach ($attributes as $key => $value) {
-            $sanitized[$key] = self::$db->escape_string($value);
-        }
-
-        return $sanitized;
+    protected static function getParamType($value): string {
+        if (is_int($value)) return 'i';
+        if (is_float($value)) return 'd';
+        return 's';
     }
 
     public function synchronize($args = []) {
@@ -108,75 +133,106 @@ class Database {
     }
 
     public static function find($id) {
-        $query = "SELECT * FROM " . static::$table . " WHERE id = $id";
-        $result = self::querySQL($query);
+        $query = "SELECT * FROM " . static::$table . " WHERE id = ?";
+        $result = self::preparedQuery($query, 'i', $id);
         return array_shift($result);
     }
 
     public static function where($column, $value, $all = false) {
-        $query = "SELECT * FROM " . static::$table . " WHERE $column = '$value'";
-        $result = self::querySQL($query);
+        $allowed = static::$columns;
+        if (!in_array($column, $allowed)) {
+            throw new \InvalidArgumentException("Columna no permitida: $column");
+        }
+        $query = "SELECT * FROM " . static::$table . " WHERE $column = ?";
+        $type = self::getParamType($value);
+        $result = self::preparedQuery($query, $type, $value);
         return $all ? $result : array_shift($result);
     }
 
     public static function pluckColumn($column, $key, $value) {
-        $query = "SELECT $column FROM " . static::$table . " WHERE $key = '$value'";
-        $result = self::$db->query($query);
-        $array = [];
+        $allowed = static::$columns;
+        if (!in_array($column, $allowed) || !in_array($key, $allowed)) {
+            throw new \InvalidArgumentException("Columna no permitida");
+        }
+        $query = "SELECT $column, $key FROM " . static::$table . " WHERE $key = ?";
+        $type = self::getParamType($value);
 
+        $stmt = self::$db->prepare($query);
+        $stmt->bind_param($type, $value);
+        $stmt->execute();
+        $result = $stmt->get_result();
+
+        $array = [];
         while ($row = $result->fetch_assoc()) {
             $array[$row[$key]] = $row[$column];
         }
+
+        $result->free();
+        $stmt->close();
 
         return $array;
     }
 
     public static function get($limit) {
-        $query = "SELECT * FROM " . static::$table . " LIMIT $limit";
-        $result = self::querySQL($query);
+        $query = "SELECT * FROM " . static::$table . " LIMIT ?";
+        $result = self::preparedQuery($query, 'i', (int) $limit);
         return $result;
     }
 
     public function create() {
-        $attributes = $this->sanitizeAttributes();
+        $attributes = $this->attributes();
+        $columns = array_keys($attributes);
+        $values = array_values($attributes);
+        $placeholders = array_fill(0, count($values), '?');
+        $types = implode('', array_map([self::class, 'getParamType'], $values));
 
         $query = "INSERT INTO " . static::$table . " (";
-        $query .= join(', ', array_keys($attributes));
-        $query .= ") VALUES ('";
-        $query .= join("', '", array_values($attributes));
-        $query .= "')";
+        $query .= join(', ', $columns);
+        $query .= ") VALUES (";
+        $query .= join(', ', $placeholders);
+        $query .= ")";
 
-        $result = self::$db->query($query);
+        $stmt = self::$db->prepare($query);
+        $stmt->bind_param($types, ...$values);
+        $result = $stmt->execute();
+        $id = self::$db->insert_id;
+        $stmt->close();
 
         return [
             'result' => $result,
-            'id' => self::$db->insert_id
+            'id' => $id
         ];
     }
 
     public function update() {
-        $attributes = $this->sanitizeAttributes();
-        $values = [];
+        $attributes = $this->attributes();
+        $columns = array_keys($attributes);
+        $values = array_values($attributes);
 
-        foreach ($attributes as $key => $value) {
-            $values[] = "{$key}='{$value}'";
-        }
+        $setClauses = array_map(fn($col) => "$col = ?", $columns);
 
         $query = "UPDATE " . static::$table . " SET ";
-        $query .= join(', ', $values);
-        $query .= " WHERE id = '" . self::$db->escape_string($this->id) . "'";
-        $query .= " LIMIT 1";
+        $query .= join(', ', $setClauses);
+        $query .= " WHERE id = ? LIMIT 1";
 
-        $result = self::$db->query($query);
+        $values[] = $this->id;
+        $types = implode('', array_map([self::class, 'getParamType'], $values));
+
+        $stmt = self::$db->prepare($query);
+        $stmt->bind_param($types, ...$values);
+        $result = $stmt->execute();
+        $stmt->close();
 
         return $result;
     }
 
     public function delete() {
-        $query = "DELETE FROM " . static::$table . " WHERE id = '" . self::$db->escape_string($this->id) . "'";
-        $query .= " LIMIT 1";
+        $query = "DELETE FROM " . static::$table . " WHERE id = ? LIMIT 1";
 
-        $result = self::$db->query($query);
+        $stmt = self::$db->prepare($query);
+        $stmt->bind_param('i', $this->id);
+        $result = $stmt->execute();
+        $stmt->close();
 
         return $result;
     }
